a40e6dac98ce27f9e18e0685f8a77278fc6ed2a3
emainguelag <110185005+emainguelag@users.noreply.github.com>
2022-11-30 13:48:59 +0100

    symfony quest 11 relations bidirectionnelles

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-
     public function index(ProgramRepository $programRepository): Response
     {
         $programs = $programRepository->findAll();
@@ -22,7 +22,6 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/show/{id}', requirements: ['id'=>'\d+'],methods: ['GET'], name: 'show')]
-
     public function show(int $id, ProgramRepository $programRepository): Response
     {
         $program = $programRepository->findOneBy(['id' => $id]);
@@ -35,6 +34,24 @@ class ProgramController extends AbstractController
         }
         return $this->render('program/show.html.twig', [
             'program' => $program,
+        ]);
+    }
+
+    #[Route('/{programId}/seasons/{seasonId}',methods: ['GET'], name: 'season_show')]
+    public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $programId]);
+        // same as $program = $programRepository->find($id);
+        $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+
+        if (!$program || !$seasonId) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
         ]);
     }
 }
